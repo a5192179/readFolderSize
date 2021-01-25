08d4ef1785202617342190afceffcc6274333d4f
Shuai Li <l>
2021-01-25 15:56:12 +0800

    first commit

---
+++ readFolderSIze.py
@@ -0,0 +1,81 @@
+import os
+import pandas as pd
+import numpy as np
+
+class Folder:
+    def __init__(self, absolutePath):
+        self.path = absolutePath
+        self.child = []
+        self.childStatic = []
+        self.size = 0
+        self.num = 0
+        self.finish = False
+        
+    def getSize(self, folderDict):
+        if self.finish == True:
+            return self.size
+        childNum = len(self.child)
+        for i in range(childNum):
+            if self.childStatic[i] == 1:
+                continue
+            self.size += folderDict[self.child[i]].getSize(folderDict)
+            self.childStatic[i] = 1
+        self.finish = True
+        print(self.path, 'size done')
+        return self.size
+
+    def addFolder(self, childFolder):
+        self.child.append(childFolder)
+        self.childStatic.append(0)
+        self.num += 1
+        self.finish = False
+
+    def addFile(self, filePath):
+        self.num += 1
+        size = os.path.getsize(filePath)
+        self.size += size
+
+
+root = 'D:/project/videoFusion/document'
+savePath = './data/test.xlsx'
+foldersList = []
+foldersList.append(root)
+folderDict = {}
+folder = Folder(root)
+folderDict[root] = folder
+while len(foldersList) > 0:
+    tempRootDir = foldersList[0]
+    files = os.listdir(tempRootDir)
+    for filename in files:
+        if filename == '.' or filename == '..':
+            print('continue:', tempRootDir + '/' + filename)
+            continue
+        filePath = tempRootDir + '/' + filename
+        relativePath = filePath[len(root + '/'):]
+        if os.path.isdir(filePath):
+            foldersList.append(filePath)
+            folder = Folder(filePath)
+            folderDict[filePath] = folder
+            folderDict[tempRootDir].addFolder(filePath)
+            # if len(relativePath.split('/')) < 4:
+            #     mainFoldersDict[filePath] = [0, 0]
+        else:
+            folderDict[tempRootDir].addFile(filePath)
+    print(tempRootDir + ' dir end')
+    foldersList.pop(0)
+
+folderNum = len(folderDict)
+print('folder num:', folderNum)
+folderDict[root].getSize(folderDict)
+
+folderInfo = pd.DataFrame(np.arange(folderNum * 3).reshape((folderNum, 3)), columns=['path', 'size', 'num'])
+i = 0
+for key in folderDict:
+    folder = folderDict[key]
+    folderInfo['path'][i] = folder.path
+    folderInfo['size'][i] = folder.size / 1024 / 1024 #MB
+    folderInfo['num'][i] = folder.num
+    i += 1
+    if i%100 == 0:
+        print('i=', i)
+folderInfo.to_excel(savePath, index=False, header=True)
---
+++ readme.md
@@ -0,0 +1,9 @@
+funtion:get the folder size(MB) file num
+
+1.set the target folder
+root = 'D:/project/videoFusion/document'
+
+2.set the output excel, the folder must be existing
+savePath = './data/test.xlsx'
+
+3.run
